57902ffee299ad1dbecd0df1cc937a0dc34377c8
arch-fan <juanserracines@gmail.com>
2024-12-27 14:11:54 +0100

    Dependency clean

--- src/main/kotlin/com/archfan/clientblock/ClientBlock.kt
+++ src/main/kotlin/com/archfan/clientblock/ClientBlock.kt
@@ -2,11 +2,7 @@ package com.archfan.clientblock
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry
 import net.fabricmc.loader.api.FabricLoader
-import net.minecraft.network.packet.BrandCustomPayload
-import net.minecraft.network.packet.CustomPayload
-import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
