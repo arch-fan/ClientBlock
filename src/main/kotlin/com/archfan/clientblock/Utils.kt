4cbcd6a805d3d8713f22651cf7f1753f8629b570
arch-fan <juanserracines@gmail.com>
2025-04-11 19:18:59 +0200

    small refactor

--- src/main/kotlin/com/archfan/clientblock/Utils.kt
+++ src/main/kotlin/com/archfan/clientblock/Utils.kt
@@ -2,9 +2,8 @@ package com.archfan.clientblock
 
 import net.fabricmc.loader.api.FabricLoader
 
-fun getModVersion(): String {
-    return FabricLoader.getInstance()
-        .getModContainer("clientblock")
-        .map { it.metadata.version.friendlyString }
-        .orElse("main")
-}
+fun getModVersion(): String = FabricLoader
+    .getInstance()
+    .getModContainer("clientblock")
+    .map { it.metadata.version.friendlyString }
+    .orElse("main")
